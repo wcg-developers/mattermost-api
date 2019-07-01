2bdbc6119ef8f3d77a88531139c73c8e1e870214
Pruthvirajsinh Zala <pruthvirajsinh@webcodegenie.com>
2019-07-01 16:18:55 +0530

    Add  icon fetures

--- README.md
+++ README.md
@@ -26,15 +26,47 @@ app.use(Mattermost({
 
 For send custom message
 ```
-Mattermost.sendmsg('Test Message');
+Mattermost.sendmsg('your message');
 ```
 
 If you want to use custom channel for send message pass option in second parameter url and username.
 If you pass this parameter library will use passed parameter other wise it will use set parameter for error.
 ```
-Mattermost.sendmsg('Test Message', {
+Mattermost.sendmsg('your message', {
    'url': 'mattermost_webhook_url',
-   'username': 'Username which display on your mattermost channel'
+   'username': 'Username which display on your mattermost channel',
+   'icon_url':'URL'(optional if you want to diffrent icon then you set in error)
+   'image_url':'URL'(optional if you want to attach image)
+});
+```
+
+For send custom message by user set these paramesters
+```
+app.use(Mattermost({
+   'url': 'mattermost_webhook_url',
+   'username': 'Username which display on your mattermost channel',
+   'userid': 'userid',
+   'password': 'password',
+   'mattermosturl': 'your mattermost URL',
+   'channelid': 'dwrgf5kzwib88gbjaozt59gpnw'
+}));
+```
+Then use this function
+
+```
+Mattermost.sendmsgByUser('your message');
+```
+
+Also you can send message with diffrent user
+```
+Mattermost.sendmsgByUser('your message', {
+        userid: 'userid',(optional or use set parameter for error)
+        password: 'password',(optional or use set parameter for error)
+        mattermosturl: 'your mattermost URL',(optional or use set parameter for error)
+        channelid: 'w9yn5153kbgzf8g5fpppu3uuqr',(optional or use set parameter for error)
+        file: data,data must be Buffer (optional if you want to upload file)
+        filename: 'abc'(must use with file)
+        image_url:'URL'(optional if you want to attach image)
 });
 ```
 
--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 'use strict'
 const request = require('request');
-var config;
+var config, userConfig = [];
 
 module.exports = mattermost
 module.exports.sendmsg = sendmsg
+module.exports.sendmsgByUser = sendmsgByUser
+
+
 /**
  * function for send error to mattermost
  * @param object of url and username 
@@ -24,14 +27,18 @@ function mattermost(option) {
             message += "Query String : \n " + JSON.stringify(req.query) + " \n";
             message += "Stack : \n ```\n " + err.stack + " \n``` \n";
             // post request for send message to mattermost
+            let body = {
+                text: message,
+                username: option.username
+            }
+            if (option.icon_url) {
+                body['icon_url'] = option.icon_url;
+            }
             request.post({
                 url: option.url,
-                body: {
-                    text: message,
-                    username: option.username
-                },
+                body: body,
                 json: true
-            }, async function (error, response, body) {
+            }, function (error, response, body) {
                 if (error) throw error;
             });
         } catch (e) {
@@ -62,17 +69,171 @@ function sendmsg(message, option = null) {
     }
     try {
         // post request for send message to mattermost
+        let body = {
+            text: message,
+            username: username
+        }
+        if (config && config.icon_url) {
+            body['icon_url'] = config.icon_url;
+        }
+        if (option != null && option.icon_url) {
+            body['icon_url'] = option.icon_url;
+        }
+        if (option && option.image_url) {
+            body['attachments'] = [{
+                "image_url": option.image_url
+            }];
+        }
         request.post({
             url: url,
-            body: {
-                text: message,
-                username: username
-            },
+            body: body,
             json: true
-        }, async function (error, response, body) {
+        }, function (error, response, body) {
             if (error) throw error;
         });
     } catch (e) {
         throw e;
     }
 }
+
+
+/**
+ * @param {object} option object contains userid, password, mattermost URL, message, channelid 
+ */
+function sendmsgByUser(message, option) {
+    let apiToken = '',
+        userid, password, mattermosturl, channelid;
+    if (!message) throw new Error('required message');
+    if (!config.userid && !option.userid) throw new Error('required userid');
+    if (!config.password && !option.password) throw new Error('required password');
+    if (!config.mattermosturl && !option.mattermosturl) throw new Error('required mattermost URL');
+    if (!config.channelid && !option.channelid) throw new Error('required channelid');
+
+    // set required parameter
+    if (option && option.userid) {
+        userid = option.userid;
+    } else {
+        userid = config.userid;
+    }
+    if (option && option.password) {
+        password = option.password;
+    } else {
+        password = config.password;
+    }
+    if (option && option.mattermosturl) {
+        mattermosturl = option.mattermosturl;
+    } else {
+        mattermosturl = config.mattermosturl;
+    }
+    if (option && option.channelid) {
+        channelid = option.channelid;
+    } else {
+        channelid = config.channelid;
+    }
+    // if user aleredy loggedin then use earlier token 
+    for (let inx in userConfig) {
+        if (userConfig[inx].userid == userid) {
+            apiToken = userConfig[inx].token;
+            break;
+        }
+    }
+    // if user is not loggedin then call api for login and set user's info in userConfig array
+    if (apiToken == '') {
+        request.post({
+            url: 'https://' + mattermosturl + '/api/v4/users/login',
+            body: {
+                login_id: userid,
+                password: password
+            },
+            json: true
+        }, function (error, response, body) {
+            if (error) throw error;
+            userConfig.push({
+                'userid': userid,
+                'password': password,
+                'token': response.headers.token
+            });
+            apiToken = response.headers.token;
+            // function for sent message
+            msgsent();
+        });
+    } else {
+        // if user alerady logged in then sent message with old token
+        msgsent();
+    }
+
+    //function for send message with optional file parameter
+    function msgsent() {
+        // if user send only message info
+        if (!option || (option && !option.file && !option.filename)) {
+            let props = {};
+            if (option && option.image_url) {
+                props['attachments'] = [{
+                    "image_url": option.image_url
+                }];
+            }
+            request.post({
+                url: 'https://' + mattermosturl + '/api/v4/posts',
+                headers: {
+                    'Authorization': 'Bearer ' + apiToken
+                },
+                body: {
+                    message: message,
+                    channel_id: channelid,
+                    props: props
+                },
+                json: true
+            }, function (error, response, body) {
+                if (error) throw error;
+            });
+        }
+        // if user send message with file
+        if (option && option.file && option.filename) {
+            // api for upload file
+            request.post({
+                url: 'https://' + mattermosturl + '/api/v4/files?channel_id=' + channelid + '&filename=' + option.filename,
+                headers: {
+                    'Authorization': 'Bearer ' + apiToken,
+                    "Content-Type": "multipart/form-data"
+                },
+                formData: {
+                    files: option.file,
+                    channel_id: channelid
+                },
+            }, function (error, response, body) {
+                if (error) {
+                    throw error
+                } else {
+                    body = JSON.parse(body);
+                    let file_ids = [];
+                    // set upload files id to create post api
+                    for (let i in body.file_infos) {
+                        file_ids[i] = body.file_infos[i].id;
+                    }
+                    // api for create post(message)
+                    let props = {};
+                    if (option && option.image_url) {
+                        props['attachments'] = [{
+                            "image_url": option.image_url
+                        }];
+                    }
+                    request.post({
+                        url: 'https://' + mattermosturl + '/api/v4/posts',
+                        headers: {
+                            'Authorization': 'Bearer ' + apiToken
+                        },
+                        body: {
+                            message: message,
+                            channel_id: channelid,
+                            file_ids: file_ids,
+                            props: props
+                        },
+                        json: true
+                    }, function (err, res, body) {
+                        if (err) throw err;
+                    });
+                }
+            });
+        }
+    }
+};
